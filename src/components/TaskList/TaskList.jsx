c93ed7ad026c3696f01f3b4e8b98a8bb68dc085b
Aman <aalix1803@gmail.com>
2024-11-27 09:47:16 +0530

    sixth

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -5,6 +5,7 @@ import CompleteTask from './CompleteTask'
 import FailedTask from './FailedTask'
 
 const TaskList = ({ data }) => {
+    // console.log(data.firstName);
     return (  
         <div id='tasklist' className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16'>
             {data.tasks.map((elem, idx) => {
