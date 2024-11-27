368ec1dbbdfe8f0c428754405d6a4d6e81ad0104
Aman <aalix1803@gmail.com>
2024-11-27 13:33:01 +0530

    eighth

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,6 +1,58 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../../context/AuthProvider';
 
 const NewTask = ({data}) => {
+    
+    
+    const [userData, setUserData] = useContext(AuthContext)
+    
+    
+    const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+    
+
+    
+    
+
+    function handleAccept() {
+        const updatedtasks = loggedInUser.data.tasks.map((elem) => {
+          if (data.taskTitle === elem.taskTitle) {
+            return {
+              ...elem,
+              newTask: false,
+              active: true,
+            };
+          }
+          return elem;
+        });
+      
+        const updatedUser = {
+          ...loggedInUser.data,
+          tasks: updatedtasks,
+        };
+      
+        // Ensure updatedData creates a new array with new references
+        const updatedData = userData.map((elem) => {
+          if (loggedInUser.data.firstName === elem.firstName) {
+            return { ...updatedUser }; // Return a new reference
+          }
+          return elem;
+        });
+      
+        setUserData([...updatedData]); // Spread to ensure new array reference
+      
+        // Update localStorage to reflect the changes
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ ...loggedInUser, data: updatedUser })
+        );
+
+        window.location.reload();
+
+      
+        console.log("Updated Data:", updatedData);
+      }
+
+
     return (
         <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
             <div className='flex justify-between items-center'>
@@ -13,7 +65,8 @@ const NewTask = ({data}) => {
             </p>
             <div className='mt-6'>
                 <button 
-                className='bg-blue-500 rounded font-medium py-1 px-2 text-xs'
+                    onClick={handleAccept}
+                    className='bg-blue-500 rounded font-medium py-1 px-2 text-xs'
                 >Accept Task
                 </button>
             </div>
